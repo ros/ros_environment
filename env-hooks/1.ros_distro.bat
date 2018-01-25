55af69a0b098602e089fcded0d6dc1ecbbb1f8bc
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-01-25 09:00:57 -0800

    switch to ament, replace todos with ardent

--- env-hooks/1.ros_distro.bat
+++
@@ -1,3 +0,0 @@
-REM copied from ros_environment/env-hooks/1.ros_distro.bat
-
-set ROS_DISTRO=TODO
--- env-hooks/1.ros_version.sh
+++
@@ -1,3 +0,0 @@
-# copied from ros_environment/env-hooks/1.ros_version.sh
-
-export ROS_VERSION=TODO
